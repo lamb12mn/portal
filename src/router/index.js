c45441acd5c1a44dc2ebb57fb244cc99cccd2542
qvwvq <qvwvq@noreply.gitcode.com>
2025-10-22 01:22:13 +0800

    2

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { nextTick } from 'vue'
 import i18n from '../i18n'
 
@@ -176,7 +176,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
